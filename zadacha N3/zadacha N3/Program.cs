e26f4c12be8b0649162cb06c048d960879d280b5
KaloyanLoboshki <48125518+KaloyanLoboshki@users.noreply.github.com>
2019-05-19 22:25:35 +0300

    jyhgjhg

---
+++ zadacha N3/zadacha N3/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace zadacha_N3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string brackets = Console.ReadLine(); 
+
+            int Nbrackets = 0;
+            int openningbrackets = 0;
+            int closingbrackets = 0;
+
+            bool properbrackets = true;
+
+            
+
+            for (int i = 0; i < brackets.Length; i++)
+            {
+                if (brackets[i] == '(' || brackets[i] == ')')
+                {
+                    if (Nbrackets == 0 && brackets[i] == ')')
+                    {
+                        properbrackets = false;
+                        return;
+                    } else if (brackets[i] == '(') {
+                        Nbrackets++;
+                        openningbrackets++;
+                    }
+                    else if (brackets[i] == ')')
+                    {
+                        Nbrackets++;
+                        closingbrackets++;
+                    }
+                    if (closingbrackets>openningbrackets)
+                    {
+                        properbrackets = false;
+                    }
+                }
+            }
+
+            if (closingbrackets != openningbrackets)
+            {
+                properbrackets = false;
+            }
+
+            if (properbrackets == false)
+            {
+                Console.WriteLine("Improper use of brackets.");
+            }
+            else if (properbrackets == true)
+            {
+                Console.WriteLine("Proper use of brackets.");
+            }
+        }
+    }
+}
---
+++ zandachaN8page106 - 5/zandachaN8page106 - 5/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace zandachaN8page106___5
+{
+    class Program
+    {
+        static void Swap(string[] masiv,int a ,int b) {
+            string c = masiv[a];
+            masiv[a] = masiv[b];
+            masiv[b] = c;
+        }
+
+        static void Main(string[] args)
+        {
+            string[] masiv = { "red", "blue", "white", "white", "white", "red", "red", "blue", "blue", "white", "white", "red", "white", "blue", "red", "blue", "white", "red" };
+
+            for (int i = 0; i < masiv.Length; i++)
+            {
+                Console.Write(masiv[i]+" ");
+            }
+            Console.WriteLine();
+
+            int redpointer = 0;
+            int whitepointer = masiv.Length - 1;
+
+            for (int i = 0; i < whitepointer+1; i++)
+            {
+                if (masiv[i] == "red" && i == redpointer)
+                {
+                    redpointer++;
+                } else if (masiv[i] == "red" && i != redpointer)
+                {
+                    Swap(masiv, i, redpointer);
+                    redpointer++;
+                }
+                else if (masiv[i] == "white" && masiv[whitepointer] == "white")
+                {
+                    i--;
+                    whitepointer--;
+                } else if (masiv[i] == "white" && masiv[whitepointer] != "white") {
+                    Swap(masiv, i, whitepointer);
+                    whitepointer--;
+                    i--;
+                }
+                
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            for (int i = 0; i < masiv.Length; i++)
+            {
+                Console.Write(masiv[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
